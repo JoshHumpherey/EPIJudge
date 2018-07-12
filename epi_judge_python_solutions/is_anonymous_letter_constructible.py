abdf2a1ce5c06a3fbc16000e78b412d7627cd24b
Joshua Humphrey <JCHumphrey@drbsystems.com>
2018-07-12 11:32:56 -0400

    Finished anonymous letter

--- epi_judge_python_solutions/is_anonymous_letter_constructible.py
+++ epi_judge_python_solutions/is_anonymous_letter_constructible.py
@@ -4,24 +4,14 @@ from test_framework import generic_test
 
 
 def is_letter_constructible_from_magazine(letter_text, magazine_text):
+    EMPTY_COUNTER = collections.Counter()
+    letterc = collections.Counter(letter_text)
+    magazinec = collections.Counter(magazine_text)
+    if (letterc-magazinec) == EMPTY_COUNTER:
+        return True
+    else:
+        return False
 
-    # Compute the frequencies for all chars in letter_text.
-    char_frequency_for_letter = collections.Counter(letter_text)
-
-    # Checks if characters in magazine_text can cover characters in
-    # char_frequency_for_letter.
-    for c in magazine_text:
-        if c in char_frequency_for_letter:
-            char_frequency_for_letter[c] -= 1
-            if char_frequency_for_letter[c] == 0:
-                del char_frequency_for_letter[c]
-                if not char_frequency_for_letter:
-                    # All characters for letter_text are matched.
-                    return True
-
-    # Empty char_frequency_for_letter means every char in letter_text can be
-    # covered by a character in magazine_text.
-    return not char_frequency_for_letter
 
 
 # Pythonic solution that exploits collections.Counter. Note that the
