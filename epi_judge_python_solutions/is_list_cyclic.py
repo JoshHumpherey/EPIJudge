8204f6186213bf8ee45285f1d8cb28928d4802c2
Joshua Humphrey <JCHumphrey@drbsystems.com>
2018-07-12 08:34:52 -0400

    Finished is_list_cyclic.py

--- epi_judge_python_solutions/is_list_cyclic.py
+++ epi_judge_python_solutions/is_list_cyclic.py
@@ -6,30 +6,14 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def has_cycle(head):
-    def cycle_len(end):
-        start, step = end, 0
-        while True:
-            step += 1
-            start = start.next
-            if start is end:
-                return step
-
-    fast = slow = head
-    while fast and fast.next and fast.next.next:
-        slow, fast = slow.next, fast.next.next
-        if slow is fast:
-            # Finds the start of the cycle.
-            cycle_len_advanced_iter = head
-            for _ in range(cycle_len(slow)):
-                cycle_len_advanced_iter = cycle_len_advanced_iter.next
-
-            it = head
-            # Both iterators advance in tandem.
-            while it is not cycle_len_advanced_iter:
-                it = it.next
-                cycle_len_advanced_iter = cycle_len_advanced_iter.next
-            return it  # iter is the start of cycle.
-    return None  # No cycle.
+    nodemap = dict()
+    while head != None:
+        if head.data in nodemap:
+            return head
+        else:
+            nodemap[head.data] = head
+        head = head.next
+    return None
 
 
 @enable_executor_hook
