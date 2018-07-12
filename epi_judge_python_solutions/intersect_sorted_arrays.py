88e924a22b050c25d4ab2d755e3ece024fdf6a3a
Joshua Humphrey <JCHumphrey@drbsystems.com>
2018-07-12 11:48:10 -0400

    Finished intersect sorted arrays

--- epi_judge_python_solutions/intersect_sorted_arrays.py
+++ epi_judge_python_solutions/intersect_sorted_arrays.py
@@ -2,8 +2,23 @@ from test_framework import generic_test
 
 
 def intersect_two_sorted_arrays(A, B):
+    results = []
+    mappingA = dict()
+    mappingB = dict()
+    for i in range(len(A)):
+        key = A[i]
+        if A[i] not in mappingA:
+            mappingA[key] = key
+    for i in range(len(B)):
+        key = B[i]
+        if B[i] not in mappingB:
+            mappingB[key] = key
+    for key in mappingA:
+        if key in mappingB:
+            results.append(key)
+    return results
 
-    return [a for i, a in enumerate(A) if (i == 0 or a != A[i - 1]) and a in B]
+
 
 
 if __name__ == '__main__':
