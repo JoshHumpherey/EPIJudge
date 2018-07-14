a9997d2bc2c0f67999d25e581a04f46f975b825f
JoshHumpherey <jhumphrey321@gmail.com>
2018-07-14 09:50:48 -0400

    Finished apply permutations

--- epi_judge_python_solutions/apply_permutation.py
+++ epi_judge_python_solutions/apply_permutation.py
@@ -1,21 +1,18 @@
 from test_framework import generic_test
 
 
-def apply_permutation(perm, A):
-
-    for i in range(len(A)):
-        # Check if the element at index i has not been moved by checking if
-        # perm[i] is nonnegative.
-        next = i
-        while perm[next] >= 0:
-            A[i], A[perm[next]] = A[perm[next]], A[i]
-            temp = perm[next]
-            # Subtracts len(perm) from an entry in perm to make it negative,
-            # which indicates the corresponding move has been performed.
-            perm[next] -= len(perm)
-            next = temp
-    # Restore perm.
-    perm[:] = [a + len(perm) for a in perm]
+def apply_permutation(perm, arr):
+    perm_map = dict()
+    for i in range(len(arr)):
+        key = perm[i]
+        value = arr[i]
+        perm_map[key] = value
+    result = []
+    for i in range(len(arr)):
+        temp = perm_map[i]
+        result.append(temp)
+    arr = result
+    return arr
 
 
 def apply_permutation_wrapper(perm, A):
