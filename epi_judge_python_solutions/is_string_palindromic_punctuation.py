01b85596d5bfe8cea7400dd0be1a870f02132c81
JoshHumpherey <jhumphrey321@gmail.com>
2018-07-14 16:06:42 -0400

    Finished palindromes

--- epi_judge_python_solutions/is_string_palindromic_punctuation.py
+++ epi_judge_python_solutions/is_string_palindromic_punctuation.py
@@ -2,19 +2,26 @@ from test_framework import generic_test
 
 
 def is_palindrome(s):
-
-    # i moves forward, and j moves backward.
-    i, j = 0, len(s) - 1
-    while i < j:
-        # i and j both skip non-alphanumeric characters.
-        while not s[i].isalnum() and i < j:
-            i += 1
-        while not s[j].isalnum() and i < j:
-            j -= 1
-        if s[i].lower() != s[j].lower():
+    str(s)
+    s = s.lower()
+    s_list = list(s)
+    space_count = 0
+    for i in range(len(s)):
+        if s_list[i].isalpha() == False:
+            s_list[i] = ''
+            space_count += 1
+    for i in range(space_count):
+        s_list.remove('')
+    ''.join(s_list)
+    palindrome_list = list(s_list)
+    print(palindrome_list)
+    back = len(palindrome_list)-1
+    for front in range(len(palindrome_list)//2):
+        if palindrome_list[front] != palindrome_list[back]:
             return False
-        i, j = i + 1, j - 1
+        back -= 1
     return True
+
 
 
 def is_palindrome_pythonic(s):
