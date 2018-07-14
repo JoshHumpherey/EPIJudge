bf83a39419e638f0ee63d614a5622ea5947da96f
JoshHumpherey <jhumphrey321@gmail.com>
2018-07-14 13:19:53 -0400

    Finished spiral matrix mostly

--- epi_judge_python_solutions/spiral_ordering_segments.py
+++ epi_judge_python_solutions/spiral_ordering_segments.py
@@ -1,27 +1,35 @@
 from test_framework import generic_test
+test_mat = [[ 1, 2, 3, 4],
+            [ 5, 6, 7, 8],
+            [ 9,10,11,12],
+            [13,14,15,16]]
 
 
 def matrix_in_spiral_order(square_matrix):
+    side_len = len(square_matrix)
+    result = []
+    for offset in range(1, (side_len//2)+1):
+        top_row = square_matrix[offset-1]
+        result.append(top_row[(offset-1):side_len-offset])
+        right_col = [col[side_len-offset] for col in square_matrix]
+        result.append(right_col[offset-1:side_len-offset])
 
-    SHIFT = ((0, 1), (1, 0), (0, -1), (-1, 0))
-    direction = x = y = 0
-    spiral_ordering = []
+        bottom_row = square_matrix[side_len-offset]
+        trimmed_bottom = bottom_row[offset:side_len-offset+1]
+        trimmed_bottom.reverse()
+        result.append(trimmed_bottom)
+        left_col = [col[offset-1] for col in square_matrix]
+        trimmed_left = left_col[offset:side_len-offset]
+        trimmed_left.reverse()
+        result.append(trimmed_left)
+        print("Count: " + str(offset))
+    return result
 
-    for _ in range(len(square_matrix)**2):
-        spiral_ordering.append(square_matrix[x][y])
-        square_matrix[x][y] = 0
-        next_x, next_y = x + SHIFT[direction][0], y + SHIFT[direction][1]
-        if (next_x not in range(len(square_matrix))
-                or next_y not in range(len(square_matrix))
-                or square_matrix[next_x][next_y] == 0):
-            direction = (direction + 1) & 3
-            next_x, next_y = x + SHIFT[direction][0], y + SHIFT[direction][1]
-        x, y = next_x, next_y
-    return spiral_ordering
+test = matrix_in_spiral_order(test_mat)
+print(test)
 
-
-if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("spiral_ordering_segments.py",
-                                       "spiral_ordering_segments.tsv",
-                                       matrix_in_spiral_order))
+#if __name__ == '__main__':
+#    exit(
+#        generic_test.generic_test_main("spiral_ordering_segments.py",
+#                                       "spiral_ordering_segments.tsv",
+#                                       matrix_in_spiral_order))
