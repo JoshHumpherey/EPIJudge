e10dae31349fc9ba5ff91b3b65ce49f8eff84de3
JoshHumpherey <jhumphrey321@gmail.com>
2018-07-15 15:59:06 -0400

    Finished merge two linked lists

--- epi_judge_python_solutions/sorted_lists_merge.py
+++ epi_judge_python_solutions/sorted_lists_merge.py
@@ -3,20 +3,19 @@ from test_framework import generic_test
 
 
 def merge_two_sorted_lists(L1, L2):
-
-    # Creates a placeholder for the result.
-    dummy_head = tail = ListNode()
-
+    placeholder = ListNode()
+    pointer = placeholder
     while L1 and L2:
         if L1.data < L2.data:
-            tail.next, L1 = L1, L1.next
+            pointer.next= L1
+            L1 = L1.next
         else:
-            tail.next, L2 = L2, L2.next
-        tail = tail.next
+            pointer.next = L2
+            L2 = L2.next
+        pointer = pointer.next
 
-    # Appends the remaining nodes of L1 or L2
-    tail.next = L1 or L2
-    return dummy_head.next
+    pointer.next = L1 or L2
+    return placeholder.next
 
 
 if __name__ == '__main__':
