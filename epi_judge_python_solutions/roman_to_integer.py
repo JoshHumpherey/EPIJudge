1ddb5af776a724bd5597eda4a1ab61b8755535e0
JoshHumpherey <jhumphrey321@gmail.com>
2018-07-15 13:27:33 -0400

    Finished Roman to Integer Conversion Problem

--- epi_judge_python_solutions/roman_to_integer.py
+++ epi_judge_python_solutions/roman_to_integer.py
@@ -2,14 +2,30 @@ import functools
 
 from test_framework import generic_test
 
+MAPPING = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
 def roman_to_integer(s):
+    if len(s) < 2:
+        return MAPPING[s]
+    roman_list = list(s)
+    total = 0
+    for i in range(len(roman_list)-1):
+        key = roman_list[i]
+        next_key = roman_list[i+1]
+        current_num = MAPPING[key]
+        next_num = MAPPING[next_key]
+        if current_num < next_num:
+            current_num = (current_num*-1)
+            total += current_num
+        elif current_num >= next_num:
+            total += current_num
 
-    T = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        if (i+1) == len(roman_list)-1:
+            total += next_num
+    return total
 
-    return functools.reduce(
-        lambda val, i: val + (-T[s[i]] if T[s[i]] < T[s[i + 1]] else T[s[i]]),
-        reversed(range(len(s) - 1)), T[s[-1]])
+
+
 
 
 if __name__ == '__main__':
