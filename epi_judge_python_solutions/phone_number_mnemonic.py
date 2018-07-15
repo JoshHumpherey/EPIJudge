bb6737a95e1b150e04769a28b89ff8d21eeefd07
JoshHumpherey <jhumphrey321@gmail.com>
2018-07-14 20:43:56 -0400

    Finished phone number problem

--- epi_judge_python_solutions/phone_number_mnemonic.py
+++ epi_judge_python_solutions/phone_number_mnemonic.py
@@ -7,20 +7,21 @@ MAPPING = ('0', '1', 'ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ')
 
 
 def phone_mnemonic(phone_number):
-    def phone_mnemonic_helper(digit):
-        if digit == len(phone_number):
-            # All digits are processed, so add partial_mnemonic to mnemonics.
-            # (We add a copy since subsequent calls modify partial_mnemonic.)
-            mnemonics.append(''.join(partial_mnemonic))
-        else:
-            # Try all possible characters for this digit.
-            for c in MAPPING[int(phone_number[digit])]:
-                partial_mnemonic[digit] = c
-                phone_mnemonic_helper(digit + 1)
+    master_perms = []
+    partial_perms = [0] * len(phone_number)
 
-    mnemonics, partial_mnemonic = [], [0] * len(phone_number)
-    phone_mnemonic_helper(0)
-    return mnemonics
+    def recursive_helper(index):
+        if index == len(phone_number):
+            result = ''.join(partial_perms)
+            master_perms.append(result)
+        else:
+            current_digit = int(phone_number[index])
+            for c in MAPPING[current_digit]:
+                partial_perms[index] = c
+                recursive_helper(index + 1)
+
+    recursive_helper(0)
+    return master_perms
 
 
 # Pythonic solution
