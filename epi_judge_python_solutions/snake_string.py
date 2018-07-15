f8da32b87245bf5d038dda688bd19c7bea24fc73
JoshHumpherey <jhumphrey321@gmail.com>
2018-07-15 14:05:16 -0400

    Finished snake string example

--- epi_judge_python_solutions/snake_string.py
+++ epi_judge_python_solutions/snake_string.py
@@ -2,18 +2,36 @@ from test_framework import generic_test
 
 
 def snake_string(s):
-
-    result = []
-    # Outputs the first row, i.e., s[1], s[5], s[9], ...
-    for i in range(1, len(s), 4):
-        result.append(s[i])
-    # Outputs the second row, i.e., s[0], s[2], s[4], ...
-    for i in range(0, len(s), 2):
-        result.append(s[i])
-    # Outputs the third row, i.e., s[3], s[7], s[11], ...
-    for i in range(3, len(s), 4):
-        result.append(s[i])
+    EDGE = 4
+    MID = 2
+    char_list = list(s)
+    list_len = len(char_list)
+    if list_len == 1:
+        return s
+    if list_len == 2:
+        char_list.reverse()
+        return ''.join(char_list)
+    top = 1
+    mid = 0
+    bot = 3
+    top_list = []
+    mid_list = []
+    bot_list = []
+    for i in range(list_len):
+        char = char_list[i]
+        if i == top:
+            top_list.append(char)
+            top += EDGE
+        if i == mid:
+            mid_list.append(char)
+            mid += MID
+        if i == bot:
+            bot_list.append(char)
+            bot += EDGE
+    result = top_list + mid_list + bot_list
     return ''.join(result)
+
+
 
 
 # Python solution uses slicing with right steps.
