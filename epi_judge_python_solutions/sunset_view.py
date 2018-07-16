c7fa5df50fb5d074417ed2f49abc71629db0503c
Joshua Humphrey <JCHumphrey@drbsystems.com>
2018-07-16 13:04:14 -0400

    Finished test cases for sunset view problem

--- epi_judge_python_solutions/sunset_view.py
+++ epi_judge_python_solutions/sunset_view.py
@@ -1,25 +1,35 @@
-import collections
-
-from test_framework import generic_test
-
+import unittest
 
 def examine_buildings_with_sunset(sequence):
+    current_max = sequence[0]
+    current_count = 1
+    for i in range(1, len(sequence)):
+        if sequence[i] > current_max:
+            current_max = sequence[i]
+            current_count += 1
+    return current_count
 
-    BuildingWithHeight = collections.namedtuple('BuildingWithHeight',
-                                                ('id', 'height'))
-    candidates = []
-    for building_idx, building_height in enumerate(sequence):
-        while candidates and building_height >= candidates[-1].height:
-            candidates.pop()
-        candidates.append(BuildingWithHeight(building_idx, building_height))
-    return [c.id for c in reversed(candidates)]
+class TestSunset(unittest.TestCase):
 
+    def test_case_1(self):
+        skyline = [1,2,3,4,5]
+        result = examine_buildings_with_sunset(skyline)
+        self.assertEqual(result, 5)
 
-def examine_buildings_with_sunset_wrapper(sequence):
-    return examine_buildings_with_sunset(iter(sequence))
+    def test_case_2(self):
+        skyline = [5,4,3,2,1]
+        result = examine_buildings_with_sunset(skyline)
+        self.assertEqual(result, 1)
 
+    def test_case_3(self):
+        skyline = [2,1,3,2,5]
+        result = examine_buildings_with_sunset(skyline)
+        self.assertEqual(result, 3)
+
+    def test_case_4(self):
+        skyline = [1,3,5,7,2,4]
+        result = examine_buildings_with_sunset(skyline)
+        self.assertEqual(result, 4)
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("sunset_view.py", 'sunset_view.tsv',
-                                       examine_buildings_with_sunset))
+    unittest.main()
