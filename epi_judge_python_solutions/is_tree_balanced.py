51814ae8d76e93237efd8e99cc6602a761046163
JoshHumpherey <jhumphrey321@gmail.com>
2018-07-16 18:23:37 -0400

    Finished balanced binary tree

--- epi_judge_python_solutions/is_tree_balanced.py
+++ epi_judge_python_solutions/is_tree_balanced.py
@@ -4,31 +4,25 @@ from test_framework import generic_test
 
 
 def is_balanced_binary_tree(tree):
+    left = dfs(tree.left, 1)
+    right = dfs(tree.right, 1)
+    print("FINAL === Left: " + str(left) + " and Right: " + str(right))
+    if abs(left-right) <= 1:
+        return True
+    else:
+        return False
 
-    BalancedStatusWithHeight = collections.namedtuple(
-        'BalancedStatusWithHeight', ('balanced', 'height'))
-
-    # First value of the return value indicates if tree is balanced, and if
-    # balanced the second value of the return value is the height of tree.
-    def check_balanced(tree):
-        if not tree:
-            return BalancedStatusWithHeight(True, -1)  # Base case.
-
-        left_result = check_balanced(tree.left)
-        if not left_result.balanced:
-            # Left subtree is not balanced.
-            return BalancedStatusWithHeight(False, 0)
-
-        right_result = check_balanced(tree.right)
-        if not right_result.balanced:
-            # Right subtree is not balanced.
-            return BalancedStatusWithHeight(False, 0)
-
-        is_balanced = abs(left_result.height - right_result.height) <= 1
-        height = max(left_result.height, right_result.height) + 1
-        return BalancedStatusWithHeight(is_balanced, height)
-
-    return check_balanced(tree).balanced
+def dfs(root, depth):
+    if root == None:
+        return depth
+    else:
+        left = dfs(root.left, depth + 1)
+        right = dfs(root.right, depth + 1)
+        print("Left: " + str(left) + " and Right: " + str(right))
+        if abs(left-right) <= 1:
+            return depth
+        else:
+            return False
 
 
 if __name__ == '__main__':
