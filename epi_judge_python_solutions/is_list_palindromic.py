e7eb58b58439eccbea289fe44f4449af5ff78d75
Joshua Humphrey <JCHumphrey@drbsystems.com>
2018-07-16 08:41:42 -0400

    Finished linked list palindrome check

--- epi_judge_python_solutions/is_list_palindromic.py
+++ epi_judge_python_solutions/is_list_palindromic.py
@@ -3,19 +3,16 @@ from test_framework import generic_test
 
 
 def is_linked_list_a_palindrome(L):
-
-    # Finds the second half of L.
-    slow = fast = L
-    while fast and fast.next:
-        fast, slow = fast.next.next, slow.next
-
-    # Compares the first half and the reversed second half lists.
-    first_half_iter, second_half_iter = L, reverse_linked_list(slow)
-    while second_half_iter and first_half_iter:
-        if second_half_iter.data != first_half_iter.data:
+    data_list = []
+    orig = L
+    while L:
+        data_list.append(L.data)
+        L = L.next
+    end = len(data_list)-1
+    for start in range(len(data_list)//2):
+        if data_list[start] != data_list[end]:
             return False
-        second_half_iter, first_half_iter = (second_half_iter.next,
-                                             first_half_iter.next)
+        end -= 1
     return True
 
 
