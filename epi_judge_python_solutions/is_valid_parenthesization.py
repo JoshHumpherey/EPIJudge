d8dcd4e50ec3e46431b833e1ce7ddc32ae0f4516
Joshua Humphrey <JCHumphrey@drbsystems.com>
2018-07-16 11:58:21 -0400

    Finished valid bracket matching

--- epi_judge_python_solutions/is_valid_parenthesization.py
+++ epi_judge_python_solutions/is_valid_parenthesization.py
@@ -2,15 +2,24 @@ from test_framework import generic_test
 
 
 def is_well_formed(s):
+    MAPPING = {'(': ')', '{': '}', '[': ']'}
+    s_list = list(s)
+    stack = []
+    for char in s_list:
+        if char in MAPPING: # must be a left bracket
+            stack.append(char)
+        else: #must be a right bracket
+            if len(stack) == 0:
+                return False
+            else:
+                char_to_match = stack.pop()
+                if MAPPING[char_to_match] != char:
+                    return False
 
-    left_chars, LOOKUP = [], {'(': ')', '{': '}', '[': ']'}
-    for c in s:
-        if c in LOOKUP:
-            left_chars.append(c)
-        elif not left_chars or LOOKUP[left_chars.pop()] != c:
-            # Unmatched right char or mismatched chars.
-            return False
-    return not left_chars
+    if len(stack) > 0: # still unmatched brackets
+        return False
+    else:
+        return True
 
 
 if __name__ == '__main__':
