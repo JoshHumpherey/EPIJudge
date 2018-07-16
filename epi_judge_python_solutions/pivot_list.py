62bea74ed805850110df838061fa81220390a107
Joshua Humphrey <JCHumphrey@drbsystems.com>
2018-07-16 08:52:22 -0400

    Finished list pivot

--- epi_judge_python_solutions/pivot_list.py
+++ epi_judge_python_solutions/pivot_list.py
@@ -6,28 +6,28 @@ from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
-def list_pivoting(l, x):
-
-    less_head = less_iter = ListNode()
-    equal_head = equal_iter = ListNode()
-    greater_head = greater_iter = ListNode()
-    # Populates the three lists.
-    while l:
-        if l.data < x:
-            less_iter.next = l
-            less_iter = less_iter.next
-        elif l.data == x:
-            equal_iter.next = l
-            equal_iter = equal_iter.next
-        else:  # l.data > x.
-            greater_iter.next = l
-            greater_iter = greater_iter.next
-        l = l.next
-    # Combines the three lists.
-    greater_iter.next = None
-    equal_iter.next = greater_head.next
-    less_iter.next = equal_head.next
-    return less_head.next
+def list_pivoting(L, pivot):
+    less = []
+    equal = []
+    more = []
+    orig = L
+    while (L != None):
+        val = L.data
+        if val < pivot:
+            less.append(val)
+        elif val > pivot:
+            more.append(val)
+        else:
+            equal.append(val)
+        L = L.next
+    result = less + equal + more
+    i = 0
+    L = orig
+    while (L != None):
+        L.data = result[i]
+        i += 1
+        L = L.next
+    return orig
 
 
 def linked_to_list(l):
