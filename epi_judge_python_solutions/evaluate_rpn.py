80ddd9ef88967f7eec2454c3130ec7afefff9f3b
Joshua Humphrey <JCHumphrey@drbsystems.com>
2018-07-16 11:40:40 -0400

    Finished RPN problem

--- epi_judge_python_solutions/evaluate_rpn.py
+++ epi_judge_python_solutions/evaluate_rpn.py
@@ -2,26 +2,25 @@ from test_framework import generic_test
 
 
 def evaluate(expression):
-
+    print()
     intermediate_results = []
-    DELIMITER = ','
     OPERATORS = {
-        '+': lambda y, x: x + y,
-        '-': lambda y, x: x - y,
-        '*': lambda y, x: x * y,
-        '/': lambda y, x: int(x / y)
+        '+': (lambda y, x: x + y),
+        '-': (lambda y, x: x - y),
+        '*': (lambda y, x: x * y),
+        '/': (lambda y, x: int(x / y))
     }
-
-    for token in expression.split(DELIMITER):
-        if token in OPERATORS:
-            intermediate_results.append(OPERATORS[token](
-                intermediate_results.pop(), intermediate_results.pop()))
-        else:  # token is a number.
-            intermediate_results.append(int(token))
+    for item in expression.split(','):
+        if item in OPERATORS:
+            num1 = intermediate_results.pop()
+            num2 = intermediate_results.pop()
+            result = OPERATORS[item](int(num1), int(num2))
+            intermediate_results.append(result)
+        else:
+            intermediate_results.append(int(item))
+    print(intermediate_results)
     return intermediate_results[-1]
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("evaluate_rpn.py", 'evaluate_rpn.tsv',
-                                       evaluate))
+    exit(generic_test.generic_test_main("evaluate_rpn.py", 'evaluate_rpn.tsv',evaluate))
