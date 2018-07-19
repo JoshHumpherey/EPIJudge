633b3d0355b0fc9f6014309d4206cd5477ce3cfb
Joshua Humphrey <JCHumphrey@drbsystems.com>
2018-07-19 14:47:58 -0400

    Finished collatz checker

--- epi_judge_python_solutions/collatz_checker.py
+++ epi_judge_python_solutions/collatz_checker.py
@@ -2,29 +2,26 @@ from test_framework import generic_test
 
 
 def test_collatz_conjecture(n):
-
-    # Stores odd numbers already tested to converge to 1.
     verified_numbers = set()
-
-    # Starts from 3, hypothesis holds trivially for 1.
+    verified_numbers.add(1)
+    verified_numbers.add(2)
     for i in range(3, n + 1):
-        sequence = set()
-        test_i = i
-        while test_i >= i:
-            if test_i in sequence:
-                # We previously encountered test_i, so the Collatz sequence has
-                # fallen into a loop. This disproves the hypothesis, so we
-                # short-circuit, returning False.
-                return False
-            sequence.add(test_i)
-
-            if test_i % 2:  # Odd number.
-                if test_i in verified_numbers:
-                    break  # test_i has already been verified to converge to 1.
-                verified_numbers.add(test_i)
-                test_i = 3 * test_i + 1  # Multiply by 3 and add 1.
+        current_test = set()
+        current_num = i
+        while(current_num <= 1):
+            if current_num == 1:
+                verified_numbers.add(i)
+                break
+            elif current_num in verified_numbers:
+                break
+            elif current_num in current_test:
+                break
+            elif current_num % 2 == 1:
+                current_num = (3 * current_test) + 1
+                current_test.add(current_num)
             else:
-                test_i //= 2  # Even number, halve it.
+                current_num = current_num // 2
+                current_test.add(current_num)
     return True
 
 
