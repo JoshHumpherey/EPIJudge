535070e79c3ef1435bb5e37c4e4e64883d14476c
Joshua Humphrey <JCHumphrey@drbsystems.com>
2018-07-19 09:08:07 -0400

    Finished LCA w/ hashmap

--- epi_judge_python_solutions/lowest_common_ancestor_close_ancestor.py
+++ epi_judge_python_solutions/lowest_common_ancestor_close_ancestor.py
@@ -6,30 +6,29 @@ from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
-def lca(node0, node1):
-
-    iter0, iter1 = node0, node1
-    nodes_on_path_to_root = set()
-    while iter0 or iter1:
-        # Ascend tree in tandem for these two nodes.
-        if iter0:
-            if iter0 in nodes_on_path_to_root:
-                return iter0
-            nodes_on_path_to_root.add(iter0)
-            iter0 = iter0.parent
-        if iter1:
-            if iter1 in nodes_on_path_to_root:
-                return iter1
-            nodes_on_path_to_root.add(iter1)
-            iter1 = iter1.parent
-    raise ValueError('node0 and node1 are not in the same tree')
+def lca(N1, N2):
+    pointer_1 = N1
+    pointer_2 = N2
+    nodeset = set()
+    while pointer_1 or pointer_2:
+        if pointer_1:
+            if pointer_1 in nodeset:
+                return pointer_1
+            nodeset.add(pointer_1)
+            pointer_1 = pointer_1.parent
+        if pointer_2:
+            if pointer_2 in nodeset:
+                return pointer_2
+            nodeset.add(pointer_2)
+            pointer_2 = pointer_2.parent
+    raise ValueError('N1 and N2 are not in the same tree')
 
 
 @enable_executor_hook
-def lca_wrapper(executor, tree, node0, node1):
+def lca_wrapper(executor, tree, N1, N2):
     result = executor.run(
-        functools.partial(lca, must_find_node(tree, node0),
-                          must_find_node(tree, node1)))
+        functools.partial(lca, must_find_node(tree, N1),
+                          must_find_node(tree, N2)))
 
     if result is None:
         raise TestFailure("Result can't be None")
