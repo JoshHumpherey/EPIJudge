5aedfd396ca0f80cdd10827fd2b3dc7e1a06f624
Joshua Humphrey <JCHumphrey@drbsystems.com>
2018-07-20 08:17:51 -0400

    Finished sort list

--- epi_judge_python_solutions/sort_list.py
+++ epi_judge_python_solutions/sort_list.py
@@ -1,21 +1,38 @@
-from sorted_lists_merge import merge_two_sorted_lists
+from list_node import ListNode
 from test_framework import generic_test
 
+def stable_sort_list(list_node):
 
-def stable_sort_list(L):
+    if not list_node or not list_node.next:
+        return list_node
 
-    # Base cases: L is empty or a single node, nothing to do.
-    if not L or not L.next:
-        return L
+    # Find the midpoint of list_node using a slow and a fast pointer.
+    pre_slow = None
+    slow = list_node
+    fast = list_node
 
-    # Find the midpoint of L using a slow and a fast pointer.
-    pre_slow, slow, fast = None, L, L
     while fast and fast.next:
         pre_slow = slow
-        fast, slow = fast.next.next, slow.next
+        fast = fast.next.next
+        slow = slow.next
     pre_slow.next = None  # Splits the list into two equal-sized lists.
-    return merge_two_sorted_lists(stable_sort_list(L), stable_sort_list(slow))
+    print("Merge sort with endpoints: " + str(list_node.data) + " and " + str(slow.data))
+    return merge_two_sorted_lists(stable_sort_list(list_node), stable_sort_list(slow))
 
+def merge_two_sorted_lists(L1, L2):
+    placeholder = ListNode()
+    pointer = placeholder
+    while L1 and L2:
+        if L1.data < L2.data:
+            pointer.next= L1
+            L1 = L1.next
+        else:
+            pointer.next = L2
+            L2 = L2.next
+        pointer = pointer.next
+
+    pointer.next = L1 or L2
+    return placeholder.next
 
 if __name__ == '__main__':
     exit(
