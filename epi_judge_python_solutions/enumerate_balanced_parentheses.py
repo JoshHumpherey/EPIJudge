a8881c7b6f0905e935f5cf0a4529f9f6ecfe22a2
JoshHumpherey <jhumphrey321@gmail.com>
2018-07-21 10:39:23 -0400

    Finished parenthesis generation

--- epi_judge_python_solutions/enumerate_balanced_parentheses.py
+++ epi_judge_python_solutions/enumerate_balanced_parentheses.py
@@ -2,24 +2,17 @@ from test_framework import generic_test, test_utils
 
 
 def generate_balanced_parentheses(num_pairs):
-    def directed_generate_balanced_parentheses(num_left_parens_needed,
-                                               num_right_parens_needed,
-                                               valid_prefix,
-                                               result=[]):
-        if num_left_parens_needed > 0:  # Able to insert '('.
-            directed_generate_balanced_parentheses(num_left_parens_needed - 1,
-                                                   num_right_parens_needed,
-                                                   valid_prefix + '(')
-        if num_left_parens_needed < num_right_parens_needed:
-            # Able to insert ')'.
-            directed_generate_balanced_parentheses(num_left_parens_needed,
-                                                   num_right_parens_needed - 1,
-                                                   valid_prefix + ')')
-        if not num_right_parens_needed:
+    result = []
+    def gen_combos(left_needed, right_needed, valid_prefix):
+        if left_needed > 0:
+            gen_combos(left_needed-1, right_needed, valid_prefix + '(')
+        if left_needed < right_needed:
+            gen_combos(left_needed, right_needed-1, valid_prefix + ')')
+        if right_needed == 0:
             result.append(valid_prefix)
-        return result
-
-    return directed_generate_balanced_parentheses(num_pairs, num_pairs, '')
+    gen_combos(num_pairs, num_pairs, '')
+    print(result)
+    return result
 
 
 def generate_balanced_parentheses_pythonic(num_pairs, num_left_open=0):
