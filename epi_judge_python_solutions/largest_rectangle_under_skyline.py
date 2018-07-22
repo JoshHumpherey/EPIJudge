6be8cae0fc4f8c6d3bc706f5addae1500a41d741
JoshHumpherey <jhumphrey321@gmail.com>
2018-07-22 14:59:16 -0400

    Finished max rectangle in skyline problem

--- epi_judge_python_solutions/largest_rectangle_under_skyline.py
+++ epi_judge_python_solutions/largest_rectangle_under_skyline.py
@@ -2,21 +2,32 @@ from test_framework import generic_test
 
 
 def calculate_largest_rectangle(heights):
+    if len(heights) == 0:
+        return 0
 
-    pillar_indices, max_rectangle_area = [], 0
-    # By appending [0] to heights, we can uniformly handle the computation for
-    # rectangle area here.
-    for i, h in enumerate(heights + [0]):
-        while pillar_indices and heights[pillar_indices[-1]] >= h:
-            height = heights[pillar_indices.pop()]
-            width = i if not pillar_indices else i - pillar_indices[-1] - 1
-            max_rectangle_area = max(max_rectangle_area, height * width)
-        pillar_indices.append(i)
-    return max_rectangle_area
+    area = lambda l,w : l*w
+    max_height = max(heights)
+    max_area = 0
+    for h in range(1, max_height + 1):
+        longest_sequence = 0
+        biggest_area = 0
+        for i in range(len(heights)):
+            if h <= heights[i]:
+                longest_sequence += 1
+                if i == len(heights) - 1:
+                    my_area = area(longest_sequence, h)
+                    if my_area > biggest_area:
+                        biggest_area = my_area
+            else:
+                my_area = area(longest_sequence, h)
+                if my_area > biggest_area:
+                    biggest_area = my_area
+                longest_sequence = 0
+        if biggest_area > max_area:
+            max_area = biggest_area
+    return max_area
+
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("largest_rectangle_under_skyline.py",
-                                       'largest_rectangle_under_skyline.tsv',
-                                       calculate_largest_rectangle))
+    exit(generic_test.generic_test_main("largest_rectangle_under_skyline.py",'largest_rectangle_under_skyline.tsv',calculate_largest_rectangle))
