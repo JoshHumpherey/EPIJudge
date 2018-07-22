40833f91852f8121d28bad7a6745e7179ec3e27e
JoshHumpherey <jhumphrey321@gmail.com>
2018-07-22 18:26:58 -0400

    Finished deadlock detection algo

--- epi_judge_python_solutions/deadlock_detection.py
+++ epi_judge_python_solutions/deadlock_detection.py
@@ -5,32 +5,33 @@ from test_framework.test_utils import enable_executor_hook
 
 
 class GraphVertex:
-
     WHITE, GRAY, BLACK = range(3)
-
     def __init__(self):
-
         self.color = GraphVertex.WHITE
-
         self.edges = []
 
 
 def is_deadlocked(graph):
-    def has_cycle(cur):
-        # Visiting a gray vertex means a cycle.
-        if cur.color == GraphVertex.GRAY:
+    WHITE, GRAY, BLACK = range(3)
+    def dfs(root):
+        if root.color == GRAY:
             return True
-
-        cur.color = GraphVertex.GRAY  # Marks current vertex as a gray one.
-        # Traverse the neighbor vertices.
-        if any(next.color != GraphVertex.BLACK and has_cycle(next)
-               for next in cur.edges):
-            return True
-        cur.color = GraphVertex.BLACK  # Marks current vertex as black.
+        else:
+            root.color = GRAY
+            for edge in root.edges:
+                temp = dfs(edge)
+                if temp:
+                    return True
+        root.color = BLACK
         return False
+    for node in graph:
+        result = dfs(node)
+        if result:
+            return True
+    return False
 
-    return any(vertex.color == GraphVertex.WHITE and has_cycle(vertex)
-               for vertex in graph)
+
+
 
 
 @enable_executor_hook
--- epi_judge_python_solutions/matrix_enclosed_regions.py
+++ epi_judge_python_solutions/matrix_enclosed_regions.py
@@ -1,21 +1,66 @@
 import collections
-
+import time
 from test_framework import generic_test
+import numpy as np
 
 
 def fill_surrounded_regions(board):
+    VISITED_MAP = np.zeros((len(board), len(board[0])))
+    print_board(board)
+    CHECK = 'W'
+    FILL = 'B'
+    for r in range(len(board)):
+        for c in range(len(board[0])):
+            if board[r][c] == CHECK:
+                dfs(board, r, c, VISITED_MAP)
+    print_board(board)
+    return board
 
-    n, m = len(board), len(board[0])
-    q = collections.deque([(i, j) for k in range(n) for i, j in (
-        (k, 0),
-        (k, m - 1))] + [(i, j) for k in range(m) for i, j in ((0, k),
-                                                              (n - 1, k))])
-    while q:
-        x, y = q.popleft()
-        if 0 <= x < n and 0 <= y < m and board[x][y] == 'W':
-            board[x][y] = 'T'
-            q.extend([(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)])
-    board[:] = [['B' if c != 'T' else 'W' for c in row] for row in board]
+
+
+def dfs(board, r, c, VISITED_MAP):
+    CHECK = 'W'
+    FILL = 'B'
+    if r > 0 and c > 0 and r >= len(board) and c >= len(board[0]):
+        if VISITED_MAP[r][c] == 1:
+            return True
+        else:
+            VISITED_MAP[r][c] = 1
+    if r < 0 or c < 0 or r >= len(board) or c >= len(board[0]):
+        return True
+    elif board[r][c] == FILL:
+        return False
+    else:
+        p1 = dfs(board, r + 1, c, VISITED_MAP)
+        if p1:
+            VISITED_MAP[r][c] = 1
+        p2 = dfs(board, r - 1, c, VISITED_MAP)
+        if p2:
+            VISITED_MAP[r][c] = 1
+        p3 = dfs(board, r, c + 1, VISITED_MAP)
+        if p3:
+            VISITED_MAP[r][c] = 1
+        p4 = dfs(board, r, c - 1, VISITED_MAP)
+        if p4:
+            VISITED_MAP[r][c] = 1
+
+        if (p1 or p2 or p3 or p4):
+            board[r][c] = CHECK
+        else:
+            board[r][c] = FILL
+
+
+
+def print_board(maze):
+    print("-----BOARD-----")
+    for r in range(len(maze)):
+        print(maze[r])
+    print()
+
+data = [['W','B','B'],
+        ['B','W','B'],
+        ['W','B','W']]
+test = fill_surrounded_regions(data)
 
 
 def fill_surrounded_regions_wrapper(board):
@@ -24,7 +69,4 @@ def fill_surrounded_regions_wrapper(board):
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("matrix_enclosed_regions.py",
-                                       'matrix_enclosed_regions.tsv',
-                                       fill_surrounded_regions_wrapper))
+    exit(generic_test.generic_test_main("matrix_enclosed_regions.py",'matrix_enclosed_regions.tsv',fill_surrounded_regions_wrapper))
