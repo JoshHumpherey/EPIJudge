36dafebbbb7050c8195cfd18c4a3e771324b2e28
JoshHumpherey <jhumphrey321@gmail.com>
2018-07-21 20:17:40 -0400

    Finished min weight in tri path

--- epi_judge_python_solutions/minimum_weight_path_in_a_triangle.py
+++ epi_judge_python_solutions/minimum_weight_path_in_a_triangle.py
@@ -4,25 +4,27 @@ from test_framework import generic_test
 
 
 def minimum_path_weight(triangle):
-
-    min_weight_to_curr_row = [0]
-    for row in triangle:
-        min_weight_to_curr_row = [
-            row[j] +
-            min(min_weight_to_curr_row[max(j - 1, 0)],
-                min_weight_to_curr_row[min(j,
-                                           len(min_weight_to_curr_row) - 1)])
-            for j in range(len(row))
-        ]
-    return min(min_weight_to_curr_row)
+    print_triangle(triangle)
+    def gen_paths(triangle, sum, index, current_col):
+        if index == len(triangle):
+            return sum
+        else:
+            left = gen_paths(triangle, sum + triangle[index][current_col], index + 1, current_col)
+            right = gen_paths(triangle, sum + triangle[index][current_col], index + 1, current_col+1)
+            return min(left, right)
 
 
+    optimal_sum = gen_paths(triangle, 0, 0, 0)
+    return optimal_sum
+
+
+def print_triangle(tri):
+    print("Triangle: " )
+    for r in range(len(tri)):
+        print(tri[r])
+    print()
 def minimum_path_weight_pythonic(triangle):
     return min(functools.reduce(lambda result, tri: [r + min(a, b) for r, a, b in zip(tri, [float('inf')] + result, result + [float('inf')])], triangle, [0]))
 
-
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("minimum_weight_path_in_a_triangle.py",
-                                       'minimum_weight_path_in_a_triangle.tsv',
-                                       minimum_path_weight))
+    exit(generic_test.generic_test_main("minimum_weight_path_in_a_triangle.py",'minimum_weight_path_in_a_triangle.tsv', minimum_path_weight))
